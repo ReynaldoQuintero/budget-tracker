ad3e83a84ec0c2e173b4c6da72215386a49f1c47
Reynaldo Quintero <reynaldoquintero@gmail.com>
2025-06-07 23:01:40 +0200

    Auth guard used according to config

--- src/views/app/guards/auth.guard.ts
+++ src/views/app/guards/auth.guard.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(): boolean {
-    return true;
-    // if (this.authService.user.user_id) {
-    //   return true;
-    // } else {
-    //   this.router.navigate(['/sign-in']);
-    //   return false;
-    // }
+    if (!environment.guards) {
+      return true;
+    }
+    if (this.authService.user.user_id) {
+      return true;
+    } else {
+      this.router.navigate(['/sign-in']);
+      return false;
+    }
   }
 }
